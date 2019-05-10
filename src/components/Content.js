cc67142fcd245492870e6174f06715745b6c2cb0
Vinicius Braga <vinicius.braga99@gmail.com>
2019-05-10 14:19:59 -0300

    Fixing footer

--- src/components/Content.js
+++ src/components/Content.js
@@ -13,9 +13,9 @@ class Content extends Component {
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
-
+{/* 
                 <div className="pure-g">
-                    {/* <div className="pure-u-1-4">
+                    <div className="pure-u-1-4">
                         <img className="pure-img-responsive" src="http://farm3.staticflickr.com/2875/9069037713_1752f5daeb.jpg" alt="Peyto Lake"/>
                     </div>
                     <div className="pure-u-1-4">
@@ -26,8 +26,8 @@ class Content extends Component {
                     </div>
                     <div className="pure-u-1-4">
                         <img className="pure-img-responsive" src="http://farm8.staticflickr.com/7357/9086701425_fda3024927.jpg" alt="Mountain"/>
-                    </div> */}
-                </div>
+                    </div>
+                </div> */}
 
                 <h2 className="content-subhead">Try Resizing your Browser</h2>
                 <p>
